486c3b6ba892d7a7d2cce61c08a80467225b3b4f
Andrew J. Korty <korty@cpan.org>
2008-02-25 13:51:29 +0000

    Import of KORTY/Authen-Krb5-Admin-0.11 from CPAN.
    gitpan-cpan-distribution: Authen-Krb5-Admin
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         KORTY/Authen-Krb5-Admin-0.11.tar.gz
    gitpan-cpan-author:       KORTY
    gitpan-cpan-maturity:     released

--- Admin.pm
+++ Admin.pm
@@ -22,7 +22,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-# $Id: Admin.pm,v 1.23 2008/01/30 12:32:22 ajk Exp $
+# $Id: Admin.pm,v 1.24 2008/02/25 13:46:54 ajk Exp $
 
 package Authen::Krb5::Admin;
 
@@ -191,7 +191,7 @@ require AutoLoader;
         KADM5_CONFIG_OLD_AUTH_GSSAPI
 );
 %EXPORT_TAGS = (constants => \@EXPORT_OK);
-$VERSION = '0.10';
+$VERSION = '0.11';
 
 # Preloaded methods go here.
 
